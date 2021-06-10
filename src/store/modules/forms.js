d86113890dd393e406b03b715c18346a0164555b
Drastyn <sheizck@gmail.com>
2021-06-10 19:13:25 -0400

    Details in image form validations

--- src/store/modules/forms.js
+++ src/store/modules/forms.js
@@ -14,14 +14,14 @@ const forms = {
   actions: {
     formsValidations({ commit }, entity) {
       let checkError = [];
+      if (!entity.file) {
+        checkError.push("Remember select your image");
+      }
       if (entity.name.length < 4 || entity.name.length > 30) {
         checkError.push("Name must be have between 4 to 30 characters");
       }
-      if (!/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/.test(entity.name)) {
+      else if (!/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/.test(entity.name)) {
         checkError.push("Name not be have especial characters");
-      }
-      if (!entity.file) {
-        checkError.push("Remeber select your image");
       }
       commit("setErrors", checkError[checkError.length - 1]);
     },
