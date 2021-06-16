1ea4d1a7a8f7c6b2b29ab1aa1000285c44f46bec
Drastyn <sheizck@gmail.com>
2021-06-16 15:39:41 -0400

    Identation to dashboard index. Changes in logOut function

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -2,49 +2,54 @@
   <div id="dashboard">
     <header class="hero is-small is-primary">
       <div class="hero-body">
-          <div class="is-flex">
-            <h1 class="title is-1">Vue Gallery</h1> 
-            <button class="button is-danger" @click="logout" style="margin-left: auto;">
-                <span class="icon">
-                  <font-awesome-icon :icon="['fas', 'sign-out-alt']"/>
-                </span>
-                <span>Exit</span>
-            </button>
-          </div>
-          <div class="credits">
-            <Credits/>
-          </div>
+        <div class="is-flex">
+          <h1 class="title is-1">Vue Gallery</h1>
+          <button
+            class="button is-danger"
+            @click="logout"
+            style="margin-left: auto;"
+          >
+            <span class="icon">
+              <font-awesome-icon :icon="['fas', 'sign-out-alt']" />
+            </span>
+            <span>Exit</span>
+          </button>
+        </div>
+        <div class="credits">
+          <Credits />
+        </div>
       </div>
     </header>
-    <router-view/>
+    <router-view />
     <GalleryFooter />
-    <FlashMessage :position="'right bottom'"/>
+    <FlashMessage :position="'right bottom'" />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import Credits from '@/components/Credits';
-import GalleryFooter from '@/components/Footer';
+import { mapState } from "vuex";
+import Credits from "@/components/Credits";
+import GalleryFooter from "@/components/Footer";
 export default {
-    name: 'dashboard',
-    components: {
-      GalleryFooter,
-      Credits,
+  name: "dashboard",
+  components: {
+    GalleryFooter,
+    Credits,
+  },
+  computed: {
+    ...mapState("notifications", ["notification"]),
+  },
+  methods: {
+    display(message) {
+      this.$store.dispatch("notifications/loginMessage", message);
+      this.flashMessage.show(this.notification);
     },
-    computed: {
-        ...mapState('notifications', ['notification']),
+    logout() {
+      this.$store.dispatch("doLogOut").then(() => {
+        this.$router.push({ name: "login_path" });
+        this.display("You have successfully logged out");
+      });
     },
-    methods: {
-        display(message) {
-            this.$store.dispatch('notifications/loginMessage', message);
-            this.flashMessage.show(this.notification);
-        },
-        logout() {
-            this.$store.dispatch('doLogOut');
-            this.$router.push({ name: 'login_path' });
-            this.display('You have successfully logged out');
-        },
-    },
-}
+  },
+};
 </script>
